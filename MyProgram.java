7ca937e1152957fe1df7d1235e7c74347c53acce
AlexBubb <159167297+AlexBubb@users.noreply.github.com>
2026-01-02 17:16:36 -0500

    Update MyProgram.java
    Moved some comments to README

--- MyProgram.java
+++ MyProgram.java
@@ -1,21 +1,3 @@
-/*
-This is a 0 knowledge proof for graph coloring
-What is graph coloring?
-It is a computer science problem where you need to color each vertice of a graph such that no two adjacent nodes
-have the same color.
-So what is a 0 knowledge proof?
-Say I have a solution to a graph coloring problem that took me a long time to find and I want to prove to you I
-solved it. The issue is I don't want to give you any information that would help you solve it yourself!
-I need a 0 knowledge proof protocol!
-This program simulates one such protocol as a conversation between person1 (the person who solved the coloring problem)
-and person2 who is checking the solution
-This protocol itself is taken from this video here: https://www.youtube.com/watch?v=Otvcbw6k4eo
-I highly recommend watching the video before diving into my practical implementation
-
-When running:
-colors.txt + edges.txt is a valid graph coloring solution
-wrongcolors.txt + edges.txt is an invalid solution
-*/
 import java.util.*;
 import java.io.*;
 public class MyProgram
@@ -34,7 +16,7 @@ public class MyProgram
     //We need to run this test multiple times to increase the odds of catching a cheater
     //Right now it's set to 100 but this number can be changed 
     //Higher numbers mean a higher chance to catch cheaters
-    //Currently with the given graph the odds that a cheater gets past are 6/7^100 or 2*10^-7
+    //Currently with the given graph the odds that a cheater gets past are 6/(7^100) or 2*10^-7
     for (int x = 0; x<100; x++) {
         //Step one is to generate a graph with all the vertices locked up
         //Let's take a look how that works (go to genLockedGraph in Solver)
@@ -55,16 +37,4 @@ public class MyProgram
     System.out.println("Congrats on your solution!");
     }
     //If they pass the tests they can be very sure we have a real solution without us ever giving any useful information
-    //I encorage you to customize the graph a little
-    //Try and feed it a wrong coloring
-    //Try and make your own
-    //Heres how to format a graph:
-    //colors.txt: each line corresponds to a vertex the format is "name color"
-    //edges.txt: each line corresponds to an edge format is "source target"
-    //make sure all edges are bidirectinal so for example if I want to connect 5 and 6 then I would put
-    //5 6
-    //and
-    //6 5
-    //A way to quickly add a new graph is to sketch it on paper first
-    //I hope you enjoyed learning about this 0 knowledge proof
 }
